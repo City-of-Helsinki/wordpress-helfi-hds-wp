d727a05799cbaf4b9be6616b3c53de4c79b6a9ba
Antti Koskinen <koskinenaa@users.noreply.github.com>
2021-09-30 14:42:35 +0300

    update integration types config

--- config/integrations/types.php
+++ config/integrations/types.php
@@ -4,11 +4,14 @@ return array(
 	'plugins' => array(
 		'contact-form-7' => array(
 			'title' => __( 'Contact Form 7', 'hds-wp' ),
-			'description' => __( 'Add HDS styling to contact forms.', 'hds-wp' ),
+			'description' => __( 'Add Helsinki styling to contact forms.', 'hds-wp' ),
 		),
 		'complianz' => array(
 			'title' => __( 'Complianz GDPR', 'hds-wp' ),
-			'description' => __( 'Add HDS styling to cookie notice.', 'hds-wp' ),
+			'description' => __( 'Add Helsinki styling to cookie notice.', 'hds-wp' ),
+			'data' => array(
+				'documents',
+			),
 		),
 		'wp-piwik' => array(
 			'title' => __( 'WP-Matomo Integration', 'hds-wp' ),
