92e8f161afb95be11eddd15fc7efa3e609a7aeca
Antti Koskinen <koskinenaa@users.noreply.github.com>
2021-11-23 11:39:41 +0200

    updated assets

--- assets/admin/js/scripts.js
+++ assets/admin/js/scripts.js
@@ -1113,3 +1113,18 @@ function hdsIcons(name) {
     edit: editBlock()
   });
 })(window.wp);
+
+wp.domReady(function () {
+  /**
+    * Buttons
+    */
+  wp.blocks.unregisterBlockStyle('core/button', 'outline');
+  wp.blocks.unregisterBlockStyle('core/button', 'fill');
+  wp.blocks.registerBlockStyle('core/button', [{
+    name: 'secondary',
+    title: 'Secondary'
+  }, {
+    name: 'supplementary',
+    title: 'Supplementary'
+  }]);
+});
