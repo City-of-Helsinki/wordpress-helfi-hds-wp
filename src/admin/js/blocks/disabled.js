435d4f7ce337b313de06a08fbf128668e46545f6
GarfTornion <billy@broomudigitals.fi>
2022-09-01 12:26:28 +0300

    feature/WY-236 re-enable code block

--- src/admin/js/blocks/disabled.js
+++ src/admin/js/blocks/disabled.js
@@ -13,9 +13,6 @@
         if (getBlockType('core/verse')) {
             unregisterBlockType( 'core/verse' );
         }
-        if (getBlockType('core/code')) {
-            unregisterBlockType( 'core/code' );
-        }
         if (getBlockType('core/cover')) {
             unregisterBlockType( 'core/cover' );
         }
