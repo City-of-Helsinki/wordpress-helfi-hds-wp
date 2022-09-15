c13ca89493edf2a3d5ae0007b81f394c547e0144
GarfTornion <billy@broomudigitals.fi>
2022-09-15 10:16:52 +0300

    feature/WY-261 restrict allowed blocks within column block

--- assets/admin/js/scripts.js
+++ assets/admin/js/scripts.js
@@ -2339,6 +2339,25 @@ wp.domReady(function () {
 });
 
 (function (wp) {
+  function addColumnAttributes(settings, name) {
+    if (typeof settings.attributes !== 'undefined') {
+      if (name == 'core/column') {
+        settings.attributes = Object.assign(settings.attributes, {
+          allowedBlocks: {
+            type: 'array',
+            default: ['core/heading', 'core/paragraph', 'core/quote', 'core/table', 'core/list', 'core/freeform', 'core/image', 'core/video', 'core/audio', 'core/file', 'core/buttons', 'core/embed']
+          }
+        });
+      }
+    }
+
+    return settings;
+  }
+
+  wp.hooks.addFilter('blocks.registerBlockType', 'column/custom-attributes', addColumnAttributes);
+})(window.wp);
+
+(function (wp) {
   function addTableAttributes(settings, name) {
     if (typeof settings.attributes !== 'undefined') {
       if (name == 'core/table') {
