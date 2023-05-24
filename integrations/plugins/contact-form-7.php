6ae383a32f86a8a796bc7e376254fa17db1a69f1
Billy Ward <96112727+BroomuBilly@users.noreply.github.com>
2023-05-24 08:17:08 +0300

    feature/WY-494 use form title as aria-label in form output, and change form lang-attribute to document's lang-attribute (#133)

--- integrations/plugins/contact-form-7.php
+++ integrations/plugins/contact-form-7.php
@@ -1,24 +1,33 @@
 <?php
 
 add_filter('wpcf7_form_elements', function($elements){
-	return str_replace(
+	$elements = str_replace(
 		array(
-			//'<p><label>',
-			//'</label></p>',
 			'class="wpcf7-form-control-wrap',
 			'</select>',
-			'<input type="file"'
 		),
 		array(
-			//'<div class="hds-text-input"><label>',
-			//'</label></div>',
 			'class="wpcf7-form-control-wrap hds-text-input__input-wrapper',
 			'</select><span class="select-chevron">' . helsinki_get_svg_icon('angle-down') . '</span>',
-			'<label class="hds-button hds-button--secondary form-file-input" tabindex="0">' . helsinki_get_svg_icon('plus') . '<span>' . __('Add file', 'hds-wp') . '</span></label><input type="file"'
 		),
 		$elements
 	);
+
+	$elements = preg_replace(
+		'/<input([^>]+)type="file"([^>]+)>/',
+		'<label class="hds-button hds-button--secondary form-file-input" tabindex="0">' . helsinki_get_svg_icon('plus') . '<span>' . __('Add file', 'hds-wp') . '</span></label><input$1type="file"$2>',
+		$elements
+	);
+
+	return $elements;
 });
+
+add_filter("shortcode_atts_wpcf7", function($out, $pairs, $atts, $shortcode){
+	if ( !$out['html_title'] ) {
+		$out['html_title'] = $out['title'];
+	}
+	return $out;
+}, 10, 4);
 
 add_filter('wpcf7_form_tag', function($scanned_tag, $replace){
 	if ( ! isset( $scanned_tag['basetype'] ) ) {
