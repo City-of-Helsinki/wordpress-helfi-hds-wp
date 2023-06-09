f3b668b034bfd2a48c693f63a441ea04d7d00d44
GarfTornion <billy@broomudigitals.fi>
2023-06-09 15:43:41 +0300

    feature/WY-332 add external link icon to facebook feed follow link

--- assets/public/js/scripts.js
+++ assets/public/js/scripts.js
@@ -265,8 +265,12 @@ hdsAccordions.init(parseAccordionElements());
       var $this = $(this);
       hideFeedFromScreenReaders($this);
       hideLinksFromScreenReaders($this);
-      var href = $this.find('a').attr('href');
-      $this.append('<a href="' + href + '" target="_blank" class="cff-follow-link hds-button hds-button--secondary">' + hds_wp["follow_on_facebook"] + '</a>');
+      var href = $this.find('.cff-author-text a').attr('href'); //if href not empty
+
+      if (href) {
+        $this.append('<a href="' + href + '" target="_blank" class="cff-follow-link hds-button hds-button--secondary">' + hds_wp["follow_on_facebook"] + hds_wp["external_link_icon"] + '</a>');
+      }
+
       var ro = new ResizeObserver(function (entries) {
         hideLinksFromScreenReaders($this);
       });
