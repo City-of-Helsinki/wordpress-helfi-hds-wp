631795a1dfd207bc5d87216f4c7b84f8e9e7532c
Billy Ward <96112727+BroomuBilly@users.noreply.github.com>
2023-08-23 10:34:44 +0300

    feature/WY-374 add custom styles and accessibility fixes to social wall (#172)

--- assets/public/js/scripts.js
+++ assets/public/js/scripts.js
@@ -279,6 +279,7 @@ hdsAccordions.init(parseAccordionElements());
 
 (function ($) {
   $(function () {
+    //Custom Facebook Feed
     $('.cff').each(function () {
       var $this = $(this);
       hideFeedFromScreenReaders($this);
@@ -293,7 +294,16 @@ hdsAccordions.init(parseAccordionElements());
         hideLinksFromScreenReaders($this);
       });
       ro.observe($this[0]);
-    });
+    }); //Social wall
+
+    $('.sb-wall').each(function () {
+      var $this = $(this);
+      hideLinksFromScreenReaders($this);
+      var ro = new ResizeObserver(function (entries) {
+        hideLinksFromScreenReaders($this);
+      });
+      ro.observe($this[0]);
+    }); //General Functions
 
     function hideLinksFromScreenReaders(element) {
       element.find('a:not(.cff-follow-link)').each(function () {
