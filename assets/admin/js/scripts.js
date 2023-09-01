cb6658032dc6a5ab7e214e94dc437e8b84e949fc
BroomuBilly <billy@broomudigitals.fi>
2023-09-01 14:57:25 +0300

    feature/WY-548 disable complianz consent area block

--- assets/admin/js/scripts.js
+++ assets/admin/js/scripts.js
@@ -1641,6 +1641,11 @@ function hdsIcons(name) {
 
     if (getBlockType('yoast/how-to-block')) {
       unregisterBlockType('yoast/how-to-block');
+    } //Disable Complianz blocks
+
+
+    if (getBlockType('complianz/consent-area')) {
+      unregisterBlockType('complianz/consent-area');
     } //Disable blocks for posts
 
 
