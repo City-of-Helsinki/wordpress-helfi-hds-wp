5649d862a3f4c240858244f8def8aed807c8f664
BroomuBilly <billy@broomudigitals.fi>
2023-09-19 08:25:59 +0300

    feature/WY-479 add photo credit text to image-banner and image-text, fix errors caused by missing srcsets in said blocks' images

--- config/blocks/callbacks.php
+++ config/blocks/callbacks.php
@@ -670,15 +670,13 @@ function hds_wp_render_image_text($attributes) {
 	$image_caption_id = hds_wp_get_random_id();
 	if (!empty($attributes['mediaId'])) {
 		$credit = hds_wp_render_credit_text($attributes['mediaId']);
-		if (!empty($credit)) {
+		if ($credit) {
 			$image_caption = sprintf(
-				'<div id="%s" class="wp-caption-text image-text-caption">%s</div>',
-				$image_caption_id,
+				'<div class="wp-caption-text image-text-caption" aria-hidden="true">%s</div>',
 				hds_wp_render_credit_text($attributes['mediaId'])
 			);
 			$image_caption_mobile = sprintf(
-				'<div id="%s" class="wp-caption-text image-text-caption image-text-caption--mobile">%s</div>',
-				$image_caption_id,
+				'<div class="wp-caption-text image-text-caption image-text-caption--mobile" aria-hidden="true">%s</div>',
 				hds_wp_render_credit_text($attributes['mediaId'])
 			);
 		}
@@ -686,12 +684,14 @@ function hds_wp_render_image_text($attributes) {
 
 	$image = '';
 	if (!empty($attributes['mediaId'])) {
+		$credit = hds_wp_render_credit_text($attributes['mediaId']);
 		$image = sprintf(
-			'<figure class="image" %s>
+			'<figure class="image">
 				%s
+				<figcaption class="screen-reader-text">%s</figcaption>
 			</figure>',
-			isset($image_caption) ? sprintf('aria-labelledby="%s"', $image_caption_id) : '',
 			wp_get_attachment_image($attributes['mediaId'], 'full', false, $imageConfig),
+			$credit ? $credit : ''
 		);
 		$wrapClasses[] = 'has-image';
 	}
@@ -741,114 +741,6 @@ function hds_wp_render_credit_text( $postId ) {
 }
 
 /**
- * Image Banner
- */
-
-function hds_wp_render_image_banner($attributes) {
-
-	$wrapClasses = array( 'wp-block-hds-wp-image-banner' );
-
-	if ($attributes['alignment'] === 'right') {
-		$wrapClasses[] = 'align-right';
-	} else {
-		$wrapClasses[] = 'align-left';
-	}
-
-	/*if (!empty($attributes['mediaId'])) {
-		$wrapClasses[] = 'has-image';
-	}
-	else {
-		$wrapClasses[] = 'has-placeholder';
-	}*/
-
-	if (!empty($attributes['className'])) {
-		$wrapClasses[] = esc_attr($attributes['className']);
-	}
-
-	$id = '';
-	if (!empty($attributes['anchor'])) {
-		$id = 'id="'.esc_attr($attributes['anchor']).'"';
-	}
-
-	$imageConfig = array(
-		'alt' => $attributes['mediaAlt'],
-		'width' => $attributes['mediaWidth'],
-		'height' => $attributes['mediaHeight'],
-		'src' => $attributes['mediaUrl'],
-		'srcset' => $attributes['mediaSrcset'],
-		'id' => $attributes['mediaId'],
-	);
-
-	$image_caption = '';
-	$image_caption_mobile = '';
-	$image_caption_id = hds_wp_get_random_id();
-	if (!empty($attributes['mediaId'])) {
-		$credit = hds_wp_render_credit_text($attributes['mediaId']);
-		if (!empty($credit)) {
-			$image_caption = sprintf(
-				'<div id="%s" class="wp-caption-text image-banner-caption">%s</div>',
-				$image_caption_id,
-				hds_wp_render_credit_text($attributes['mediaId'])
-			);
-			$image_caption_mobile = sprintf(
-				'<div id="%s" class="wp-caption-text image-banner-caption image-banner-caption--mobile">%s</div>',
-				$image_caption_id,
-				hds_wp_render_credit_text($attributes['mediaId'])
-			);
-
-		}
-	}
-
-	$image = '';
-	if (!empty($attributes['mediaId'])) {
-		$image = sprintf(
-			'<figure class="image" %s>
-				%s
-			</figure>',
-			isset($image_caption) ? sprintf('aria-labelledby="%s"', $image_caption_id) : '',
-			wp_get_attachment_image($attributes['mediaId'], 'full', false, $imageConfig),
-		);
-		$wrapClasses[] = 'has-image';
-	}
-	else {
-		$image = '<div class="image"><div class="placeholder"></div></div>';
-		$wrapClasses[] = 'has-placeholder';
-	}
-
-	$content = '';
-	if (!empty($attributes['contentTitle']) || !empty($attributes['contentText']) || !empty($attributes['buttonText'])) {
-		$content = sprintf(
-			'<div class="content">
-				<div class="content__inner">
-					%s
-					%s
-					%s
-				</div>
-			</div>',
-			!empty($attributes['contentTitle']) ? sprintf('<h2 class="content__heading">%s</h2>', $attributes['contentTitle']) : '',
-			!empty($attributes['contentText']) ? sprintf('<div class="content__text">%s</div>', wpautop($attributes['contentText'], false)) : '',
-			!empty($attributes['buttonText']) && !empty($attributes['buttonUrl']) ? sprintf('<a class="content__link hds-button hds-button--primary" href="%s" %s>%s</a>', $attributes['buttonUrl'], $attributes['targetBlank'] ? 'target="_blank"' : '', $attributes['buttonText']) : ''
-		);
-	}
-
-	return sprintf(
-		'<div %s class="%s">
-			%s
-			%s
-			%s
-			%s
-		</div>',
-		$id,
-		implode( ' ', $wrapClasses ),
-		$image_caption_mobile,
-		$image,
-		$image_caption,
-		$content
-	);
-
-}
-
-/**
  * Image & Banner
  */
 
@@ -880,11 +772,32 @@ function hds_wp_render_image_banner($attributes) {
 		'id' => $attributes['mediaId'],
 	);
 
+	$image_caption = '';
+	$image_caption_mobile = '';
+	if (!empty($attributes['mediaId'])) {
+		$credit = hds_wp_render_credit_text($attributes['mediaId']);
+		if ($credit) {
+			$image_caption = sprintf(
+				'<div class="wp-caption-text image-banner-caption" aria-hidden="true">%s</div>',
+				hds_wp_render_credit_text($attributes['mediaId'])
+			);
+			$image_caption_mobile = sprintf(
+				'<div class="wp-caption-text image-banner-caption image-banner-caption--mobile" aria-hidden="true">%s</div>',
+				hds_wp_render_credit_text($attributes['mediaId'])
+			);
+
+		}
+	}
+
 	$image = '';
 	if (!empty($attributes['mediaId'])) {
+		$credit = hds_wp_render_credit_text($attributes['mediaId']);
 		$image = sprintf(
-			'<div class="image">%s</div>',
-			wp_get_attachment_image($attributes['mediaId'], 'full', false, $imageConfig)
+			'<figure class="image">%s
+				<figcaption class="screen-reader-text">%s</figcaption>
+			</figure>',
+			wp_get_attachment_image($attributes['mediaId'], 'full', false, $imageConfig),
+			$credit ? $credit : ''
 		);
 		$wrapClasses[] = 'has-image';
 	}
@@ -911,15 +824,19 @@ function hds_wp_render_image_banner($attributes) {
 
 	return sprintf(
 		'<div %s class="%s">
-		  <div class="image-banner--wrapper">
+			%s
+			<div class="image-banner--wrapper">
 				%s
 				%s
 			</div>
+			%s
 		</div>',
 		$id,
 		implode( ' ', $wrapClasses ),
+		$image_caption_mobile,
 		$image,
-		$content
+		$content,
+		$image_caption
 	);
 
 }
