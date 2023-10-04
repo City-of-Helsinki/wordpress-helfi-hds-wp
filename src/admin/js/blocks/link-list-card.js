d8523c45e88d17b965875ae2630c1fd2b41a2cae
BroomuBilly <billy@broomudigitals.fi>
2023-10-04 14:39:27 +0300

    feature/WY-303 update link-list-cards block icons

--- src/admin/js/blocks/link-list-card.js
+++ src/admin/js/blocks/link-list-card.js
@@ -23,7 +23,7 @@
     registerBlockType('hds-wp/link-list-card', {
       apiVersion: 2,
       title: __('Helsinki - Link List Card', 'hds-wp'),
-      icon: 'location-alt',
+      icon: 'screenoptions',
       category: 'hds-wp',
       style: 'hds-map',
       parent: ['hds-wp/link-list-cards'],
