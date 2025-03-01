a64519fd89c058112e5298989a63eb4ead769b2f
Antti Koskinen <koskinenaa@users.noreply.github.com>
2025-03-01 20:27:20 +0200

    Update Assets.php
    replace foreign function calls with a filter

--- class/Assets.php
+++ class/Assets.php
@@ -136,17 +136,11 @@ class Assets extends Module {
 			$this->assetVersion( $this->assetPath('admin', 'styles', $this->minified, 'css') ),
 			'all'
 		);
-		if (function_exists('helsinki_theme_mod') && function_exists('helsinki_scheme_root_styles')) {
-			$current_scheme = helsinki_theme_mod('helsinki_general_style', 'scheme');
-			if (function_exists('helsinki_default_scheme')) {
-				$current_scheme = $current_scheme ?: helsinki_default_scheme();
-			}
-			ob_start();
-			helsinki_scheme_root_styles($current_scheme);
-			$inlineStyle = ob_get_clean();
-			wp_add_inline_style('helsinki-wp-admin-styles', $inlineStyle);
-		}
 
+		$inline_styles = apply_filters( 'helsinki_wp_admin_inline_styles', '' );
+		if ( $inline_styles ) {
+			wp_add_inline_style( 'helsinki-wp-admin-styles', $inline_styles );
+		}
 	}
 
 	public function publicScripts() {
