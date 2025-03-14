2723c3f0f74d9f01498a737a1c787d2dbcc4075e
Antti Koskinen <koskinenaa@users.noreply.github.com>
2025-03-14 21:24:20 +0200

    Update contact-form-7.php

--- integrations/plugins/contact-form-7.php
+++ integrations/plugins/contact-form-7.php
@@ -1,5 +1,121 @@
 <?php
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+add_filter( 'wpcf7_is_date', 'custom_email_confirmation_validation_filter', 10, 2 );
+
+function custom_email_confirmation_validation_filter( $result, $text ) {
+	$result = preg_match(
+  	  '/^([0-9]{1,2})\.([0-9]{1,2})\.([0-9]{4})$/',
+  	  $text,
+  	  $matches
+    );
+
+    if ( $result ) {
+  	  $result = checkdate( $matches[2], $matches[1], $matches[3] );
+    }
+
+    return $result;
+}
+
+add_action( 'wpcf7_init', 'dasafsdafsdasfdafsd', 11, 0 );
+
+function dasafsdafsdasfdafsd() {
+	wpcf7_remove_form_tag( 'date*' );
+	wpcf7_remove_form_tag( 'date' );
+
+	wpcf7_add_form_tag( array( 'date', 'date*' ),
+		'hds_react_date_form_tag_handler',
+		array(
+			'name-attr' => true,
+		)
+	);
+
+	\add_action( 'wp_enqueue_scripts', 'sadfafsafsdasdf', 5 );
+}
+
+function sadfafsafsdasdf() {
+	wp_register_script(
+        'hds-react-date-picker',
+        \ArtCloud\Helsinki\Plugin\HDS\plugin_url() . 'assets/react/date-picker.js',
+        array(),
+        time(),
+        array( 'strategy' => 'defer', 'in_footer' => true )
+    );
+
+	do_action( 'helsinki_wp_add_hds_react_dependency', 'hds-react-date-picker' );
+}
+
+function hds_react_date_form_tag_handler( $tag ) {
+	if ( empty( $tag->name ) ) {
+		return '';
+	}
+
+	do_action( 'helsinki_wp_enqueue_hds_react' );
+
+	$atts = helsinki_wp_map_cf7_date_input_config($tag);
+
+    return sprintf(
+        '<span class="wpcf7-form-control-wrap" data-hds-react="DatePicker" data-hds-component="%2$s"></span>',
+		esc_attr( $tag->name ),
+        htmlspecialchars(json_encode($atts), ENT_QUOTES, 'UTF-8')
+    );
+}
+
+function helsinki_wp_map_cf7_date_input_config( $tag ): array {
+	return array(
+		'config' => array(
+	        'submitEvent' => 'wpcf7submit',
+	        'value' => helsinki_wp_format_cf7_date_input_value( $tag ),
+	    ),
+	    'input' => array(
+	        // Input
+	        'autocomplete' => $tag->get_option( 'autocomplete', '[-0-9a-zA-Z]+', true ),
+	        'className' => 'hds-react-component hds-react-component__date-input',
+	        'id' => $tag->get_id_option(),
+	        'name' => $tag->name,
+	        'readonly' => $tag->has_option( 'readonly' ),
+	        'required' => $tag->is_required(),
+	        'tabindex' => $tag->get_option( 'tabindex', 'signed_int', true ),
+
+	        // Date input
+	        'helperText' => esc_html_x( 'Use format D.M.YYYY', 'DateInput helperText', 'hds-wp' ),
+	        'errorText' => '',
+	        'successText' => '',
+	        'infoText' => '',
+	        'initialMonth' => '',
+	        'label' => esc_html_x( 'Choose a date', 'DateInput label', 'hds-wp' ),
+	        'language' => apply_filters( 'helsinki_wp_current_language', 'en' ),
+	        'minDate' => $tag->get_date_option( 'min' ),
+	        'maxDate' => $tag->get_date_option( 'max' ),
+	        'initialMonth' => '',
+	        'disableDatePicker' => false,
+	        'disableConfirmation' => false,
+	    ),
+	);
+}
+
+function helsinki_wp_format_cf7_date_input_value( $tag ): string {
+    $value = (string) reset( $tag->values );
+
+    $value = $tag->get_default_option( $value );
+
+	if ( $value ) {
+		$datetime_obj = date_create_immutable(
+			preg_replace( '/[_]+/', ' ', $value ),
+			wp_timezone()
+		);
+
+		if ( $datetime_obj ) {
+			$value = $datetime_obj->format( 'j.n.Y' );
+		}
+	}
+
+	return wpcf7_get_hangover( $tag->name, $value );
+}
+
 add_filter('wpcf7_form_elements', function($elements){
 	$elements = str_replace(
 		array(
