c2f30183f13e5853dc62eaa7052bf98f6a2b903e
Antti Koskinen <koskinenaa@users.noreply.github.com>
2025-03-17 15:34:37 +0200

    Update rollup.config.mjs

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,7 @@ import nodeResolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import babel from '@rollup/plugin-babel';
 import replace from '@rollup/plugin-replace';
+import terser from '@rollup/plugin-terser';
 
 export default {
   input: 'src/react/components.js',
@@ -9,6 +10,7 @@ export default {
     dir: 'assets/react',
     format: 'iife',
     name: 'library',
+    compact: true,
     sourcemap: false,
     globals: {
       lodash: 'lodash',
@@ -29,7 +31,8 @@ export default {
     replace({
       preventAssignment: false,
       'process.env.NODE_ENV': '"development"'
-    })
+    }),
+    terser()
   ],
   external: [
     'react',
