1e4c72a66e9c1ac246c83740c4b9db6a892942f5
Antti Koskinen <koskinenaa@users.noreply.github.com>
2025-06-19 17:17:29 +0300

    css

--- assets/admin/js/scripts.js
+++ assets/admin/js/scripts.js
@@ -3461,13 +3461,6 @@ function hdsIcons(name) {
   }
 })(window.wp);
 wp.domReady(function () {
-  /* Disable default formats */
-  wp.richText.unregisterFormatType('core/image');
-  wp.richText.unregisterFormatType('core/text-color');
-  wp.richText.unregisterFormatType('core/keyboard');
-  wp.richText.unregisterFormatType('core/code');
-});
-wp.domReady(function () {
   /**
     * Buttons
     */
@@ -3627,6 +3620,13 @@ wp.domReady(function () {
   }, 'tableEditorWrapperExtraClass');
   wp.hooks.addFilter('editor.BlockListBlock', 'table/custom-editor-wrapper-class', tableEditorWrapperExtraClass);
 })(window.wp);
+wp.domReady(function () {
+  /* Disable default formats */
+  wp.richText.unregisterFormatType('core/image');
+  wp.richText.unregisterFormatType('core/text-color');
+  wp.richText.unregisterFormatType('core/keyboard');
+  wp.richText.unregisterFormatType('core/code');
+});
 (function (wp) {
   /* inspired from https://github.com/Yoast/wpseo-woocommerce/blob/trunk/js/src/yoastseo-woo-replacevars.js */
   /* global jQuery, YoastSEO, app, globals YoastACFAnalysisConfig */
