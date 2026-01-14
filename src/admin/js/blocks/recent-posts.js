e55e7cca3d8e5b07b6d62efea20fd01a0a14c09d
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-01-14 10:38:14 +0200

    Update recent-posts.js

--- src/admin/js/blocks/recent-posts.js
+++ src/admin/js/blocks/recent-posts.js
@@ -18,6 +18,10 @@
     return [
       {label: 4, value: 4},
       {label: 8, value: 8},
+      {label: 12, value: 12},
+      {label: 16, value: 16},
+      {label: 20, value: 20},
+      {label: 24, value: 24},
     ];
   }
 
