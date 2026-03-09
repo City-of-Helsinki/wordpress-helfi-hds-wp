3278f0f42cb6ac293401ceac0e2fff39c4a38984
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-03-09 12:14:55 +0200

    Update complianz-gdpr.php

--- integrations/plugins/complianz-gdpr.php
+++ integrations/plugins/complianz-gdpr.php
@@ -10,6 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 \add_filter( 'cmplz_integrations', __NAMESPACE__ . '\\provide_cmplz_integration' );
 \add_filter( 'cmplz_integration_path', __NAMESPACE__ . '\\provide_cmplz_integration_path', 10, 2 );
+\add_filter( 'cmplz_option_blocked_content_text', __NAMESPACE__ . '\\provide_blocked_content_text', 10 );
 
 function cmplz_integration_name(): string {
 	return 'powerbi';
@@ -31,4 +32,8 @@ function provide_cmplz_integration_path( string $path, string $integration ): st
 	}
 
 	return $path;
+}
+
+function provide_blocked_content_text( string $text ): string {
+	return __( 'Please accepta "{category}" cookies to view this content.', 'hds-wp' );
 }
