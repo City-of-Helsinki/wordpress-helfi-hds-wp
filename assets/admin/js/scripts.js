3ad90fb8079e2866fd511f033e2c41783d45a353
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-03-25 17:06:29 +0200

    js

--- assets/admin/js/scripts.js
+++ assets/admin/js/scripts.js
@@ -4052,7 +4052,6 @@ wp.domReady(function () {
 })(window.wp);
 wp.domReady(function () {
   /* Disable default formats */
-  wp.richText.unregisterFormatType('core/image');
   wp.richText.unregisterFormatType('core/text-color');
   wp.richText.unregisterFormatType('core/keyboard');
   wp.richText.unregisterFormatType('core/code');
