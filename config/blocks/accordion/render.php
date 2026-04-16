24a62a91e97eab6ad7f926b6e5734c80d411c46a
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-04-16 18:09:58 +0300

    Update render.php

--- config/blocks/accordion/render.php
+++ config/blocks/accordion/render.php
@@ -8,22 +8,25 @@ use ArtCloud\Helsinki\Plugin\HDS\Svg;
 
 function hds_wp_render_block_accordion($attributes, $content)
 {
-	if (!isset($attributes['blockVersion']) || $attributes['blockVersion'] <= 1) {
+	if (
+		! isset( $attributes['blockVersion'] )
+		|| $attributes['blockVersion'] <= 1
+	) {
 		return $content;
 	}
 
 	$html = '';
-	if (!empty($attributes['title'])) {
+	if ( ! empty( $attributes['title'] ) ) {
 		$html .= sprintf(
 			'<h2 class="accordion_heading">%s</h2>',
-			esc_html($attributes['title'])
+			esc_html( $attributes['title'] )
 		);
 	}
 
-	if (!empty($attributes['description'])) {
+	if ( ! empty( $attributes['description'] ) ) {
 		$html .= sprintf(
 			'<div class="accordion-description">%s</div>',
-			wpautop($attributes['description'], false)
+			hds_wp_block_text_kses( wpautop( $attributes['description'], false ) )
 		);
 	}
 
@@ -49,43 +52,57 @@ function hds_wp_render_block_accordion($attributes, $content)
 	);
 }
 
-function hds_wp_render_block_accordion_panel($attributes)
-{
+function hds_wp_render_block_accordion_panel( array $attributes ): string {
+	$headings = array(
+		'h2' => true,
+		'h3' => true,
+		'h4' => true,
+		'h5' => true,
+		'h6' => true,
+	);
+
+	if (
+		isset( $attributes['headingLevel'] )
+		&& isset( $headings[ $attributes['headingLevel'] ] )
+	) {
+		$heading_level = $attributes['headingLevel'];
+	} else {
+		$heading_level = 'h2';
+	}
+
 	$title = sprintf(
 		'<%1$s class="accordion__title">
-				<button id="panel-toggle-%2$s" class="accordion__toggle" type="button" aria-controls="panel-%2$s" aria-expanded="false">
-					%3$s
-					<span class="accordion__icon">%4$s</span>
-				</button>
-			</%1$s>',
-		isset($attributes['headingLevel']) ? $attributes['headingLevel'] : 'h2',
-		$attributes['blockId'],
-		hds_wp_render_block_accordion_panel_title( $attributes ),
-		Svg::icon('arrows-operators', 'angle-up')
+			<button id="panel-toggle-%2$s" class="accordion__toggle" type="button" aria-controls="panel-%2$s" aria-expanded="false">
+				%3$s
+				<span class="accordion__icon">%4$s</span>
+			</button>
+		</%1$s>',
+		$heading_level,
+		esc_attr( $attributes['blockId'] ),
+		hds_wp_block_text_kses( $attributes['panelTitle'] ?? __( 'Panel Title', 'hds-wp' ) ),
+		Svg::icon( 'arrows-operators', 'angle-up' )
 	);
 
 	$panel = sprintf(
 		'<div id="panel-%1$s" class="accordion__panel" aria-labelledby="panel-toggle-%1$s" role="region" hidden="true">
-				<div class="accordion__content">%2$s</div>
-				<button class="accordion__close hds-button hds-button--supplementary" type="button">
-					<span>%3$s</span>
-					%4$s
-				</button>
-			</div>',
-		$attributes['blockId'],
-		apply_filters('the_content', isset($attributes['innerContent']) ? $attributes['innerContent'] : ''),
-		__('Close', 'hds-wp'),
-		Svg::icon('arrows-operators', 'angle-up')
+			<div class="accordion__content">%2$s</div>
+			<button class="accordion__close hds-button hds-button--supplementary" type="button">
+				<span>%3$s</span>
+				%4$s
+			</button>
+		</div>',
+		esc_attr( $attributes['blockId'] ),
+		apply_filters( 'the_content', wp_kses_post( $attributes['innerContent'] ?? '' ) ),
+		esc_html__( 'Close', 'hds-wp' ),
+		Svg::icon( 'arrows-operators', 'angle-up' )
 	);
 
-	$wrapClasses = array('wp-block-hds-wp-accordion-panel', 'accordion__section');
-	if (!empty($attributes['className'])) {
-		$wrapClasses[] = esc_attr($attributes['className']);
+	$wrapClasses = array( 'wp-block-hds-wp-accordion-panel', 'accordion__section' );
+	if ( ! empty( $attributes['className'] ) ) {
+		$wrapClasses[] = esc_attr( $attributes['className'] );
 	}
 
-	$html_attr = array(
-		'class' => implode( ' ', $wrapClasses ),
-	);
+	$html_attr = array( 'class' => implode( ' ', $wrapClasses ) );
 
 	if ( ! empty( $attributes['anchor'] ) ) {
 		$html_attr['id'] = $attributes['anchor'];
@@ -100,24 +117,4 @@ function hds_wp_render_block_accordion_panel($attributes)
 		$title,
 		$panel
 	);
-}
-
-function hds_wp_render_block_accordion_panel_title( array $attributes ): string {
-	if ( empty( $attributes['panelTitle'] ) ) {
-		return '';
-	}
-
-	return wp_kses( $attributes['panelTitle'], array(
-		'img' => array(
-			'alt' => true,
-			'class' => true,
-			'height' => true,
-			'loading' => true,
-			'sizes' => true,
-			'src' => true,
-			'srcset' => true,
-			'style' => true,
-			'width' => true,
-		),
-	) );
 }
