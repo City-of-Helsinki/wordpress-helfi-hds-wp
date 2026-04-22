6c818c15e1fddaba2a0905b9e00b0834252a34a0
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-04-22 11:37:48 +0300

    Update BlockEditor.php

--- class/BlockEditor.php
+++ class/BlockEditor.php
@@ -6,10 +6,66 @@ if ( ! defined( 'ABSPATH' ) ) {
   exit;
 }
 
+use WP_Theme_JSON_Data;
+
 class BlockEditor extends Module
 {
 	public function init()
 	{
+		\add_filter( 'wp_theme_json_data_theme', array( $this, 'theme_json' ) );
+		\add_filter( 'register_block_type_args', array( $this, 'disable_block_alignment' ), 10, 2 );
+		\add_filter( 'block_bindings_supported_attributes', '__return_empty_array' );
+	}
 
+	function disable_block_alignment( array $args, string $block_type ): array
+	{
+		if ( substr( $block_type, 0, 5 ) === 'core/'
+			&& isset( $args['supports']['align'] )
+		) {
+			$args['supports']['align'] = false;
+		}
+
+		return $args;
+	}
+
+	public function theme_json( WP_Theme_JSON_Data $theme_json ): WP_Theme_JSON_Data
+	{
+		$new_data = array_reduce(
+			$this->theme_json_modifiers(),
+			function( $theme_json, $modifier ) {
+				$this->$modifier( $theme_json );
+
+				return $theme_json;
+			},
+			array(
+				'version' => 3,
+				'settings' => array(),
+				'styles' => array(),
+			)
+		);
+
+		return $theme_json->update_with( $new_data );
+	}
+
+	private function theme_json_modifiers(): array
+	{
+		return array(
+			'settings_layout',
+			'settings_shadow',
+		);
+	}
+
+	private function settings_layout( array &$theme_json ): void
+	{
+		$theme_json['settings']['layout'] = array(
+			'allowEditing' => false,
+		);
+	}
+
+	private function settings_shadow( array &$theme_json ): void
+	{
+		$theme_json['settings']['shadow'] = array(
+			'defaultPresets' => false,
+		);
 	}
 }
