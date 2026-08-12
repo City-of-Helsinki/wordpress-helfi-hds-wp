81345fafbc5ae8a5c0faa23d36ca2ba1cf3e160b
Antti Koskinen <koskinenaa@users.noreply.github.com>
2026-08-12 15:44:33 +0300

    Create maintenance.php

---
+++ features/maintenance/template/maintenance.php
@@ -0,0 +1,82 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+use function ArtCloud\Helsinki\Plugin\HDS\Features\Maintenance\create_maintenance_page;
+use function ArtCloud\Helsinki\Plugin\HDS\Features\Maintenance\create_maintenance_page_meta;
+
+$maintenance = create_maintenance_page();
+$meta = create_maintenance_page_meta( $maintenance );
+
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+	<head>
+		<title><?php echo esc_html( $maintenance->site_title() ); ?></title>
+		<meta charset="<?php echo esc_attr( $maintenance->charset() ); ?>" />
+		<meta name="viewport" content="width=device-width, maximum-scale=1, initial-scale=1, minimum-scale=1">
+		<meta name="description" content="<?php echo esc_attr( $maintenance->site_description() ); ?>"/>
+		<meta http-equiv="X-UA-Compatible" content="" />
+
+		<?php
+
+			foreach ( $meta->properties() as $property => $content ) {
+				printf(
+					'<meta property="%s" content="%s" />',
+					esc_attr( $property ),
+					parse_url( $content, PHP_URL_HOST ) ? esc_url( $content ) : esc_attr( $content )
+				);
+			}
+
+		?>
+
+		<?php do_action( 'helsinki_maintenance_head' ); ?>
+	</head>
+
+	<body class="maintenance">
+
+		<?php do_action( 'helsinki_maintenance_top', $maintenance ); ?>
+
+		<header id="masthead">
+			<?php do_action( 'helsinki_maintenance_header_top', $maintenance ); ?>
+
+			<div class="hds-container">
+			  <?php do_action( 'helsinki_maintenance_header', $maintenance ); ?>
+			</div>
+
+			<?php do_action( 'helsinki_maintenance_header_bottom', $maintenance ); ?>
+		</header>
+
+		<main id="main">
+			<?php do_action( 'helsinki_maintenance_main_top', $maintenance ); ?>
+
+		    <div class="content">
+
+				<div class="hds-container content__container">
+
+					<div class="content__main">
+						<?php do_action( 'helsinki_maintenance_main', $maintenance ); ?>
+					</div>
+
+				</div>
+		    </div>
+
+			<?php do_action( 'helsinki_maintenance_main_bottom', $maintenance ); ?>
+		</main>
+
+		<footer id="footer">
+			<?php do_action( 'helsinki_maintenance_footer_top', $maintenance ); ?>
+
+		    <div class="hds-container">
+				<?php do_action( 'helsinki_maintenance_footer', $maintenance ); ?>
+		    </div>
+
+			<?php do_action( 'helsinki_maintenance_footer_bottom', $maintenance ); ?>
+		</footer>
+
+  		<?php do_action( 'helsinki_maintenance_bottom', $maintenance ); ?>
+
+	</body>
+</html>
